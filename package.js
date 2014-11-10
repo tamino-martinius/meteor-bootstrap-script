567e2650918713a7c75d6790b1fbb72046b1b448
Tamino Martinius <dev@zaku.eu>
2014-11-10 11:04:27 +0100

    bugfix - load script on client only

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   name: 'zaku:bootstrap-script',
   summary: 'Front-end framework from Twitter - script only',
-  version: '0.9.0',
+  version: '0.9.0_1',
   git: 'https://github.com/Zaku-eu/meteor-bootstrap-script.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.addFiles('zaku:bootstrap-script.js');
+  api.addFiles('zaku:bootstrap-script.js', 'client');
 });
