b10fc37c31d8da67312bfba3c3af1fe861c40155
Tamino Martinius <dev@zaku.eu>
2014-11-10 14:41:33 +0100

    Revert bee4615..567e265
    This rolls back to commit bee4615c96c35f17c3f649104b55bced5345f702.

--- package.js
+++ package.js
@@ -1,11 +1,10 @@
 Package.describe({
-  name: 'zaku:bootstrap-script',
-  summary: 'Front-end framework from Twitter - script only',
-  version: '0.9.0_1',
-  git: 'https://github.com/Zaku-eu/meteor-bootstrap-script.git'
+  summary: "Front-end framework from Twitter - script only"
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.0');
-  api.addFiles('zaku:bootstrap-script.js', 'client');
+Package.on_use(function (api) {
+  api.use('jquery');
+
+  var path = Npm.require('path');
+  api.add_files(path.join('js', 'bootstrap.js'), 'client');
 });
